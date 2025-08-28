a1fb2c5aa18106b5ff6590fbc8da1b8e0e89fa21
jreznick <jreznick@gmail.com>
2025-08-28 08:42:03 -0400

    Update crypto_s3.py

--- services/web/project/crypto_s3.py
+++ services/web/project/crypto_s3.py
@@ -11,9 +11,6 @@ ROLE_SESSION_NAME = ''
 
 
 class S3Session:
-    """
-    Manage access to reads & writes on an S3 bucket
-    """
     def __init__(self):
         session = boto3.session.Session()
         sts_client = session.client('sts')
@@ -30,9 +27,6 @@ class S3Session:
 
 
 class EMPIEncryptor:
-    """
-    Create and manage a Fernet key, encrypt and decrypt files sharing the key.
-    """
     def __init__(self, s3_session):
         self.s3_session = s3_session
 
@@ -93,12 +87,6 @@ class EMPIEncryptor:
 
 
 class EMPIFileCrypt:
-    """
-    This is a context manager which encrypts EMPI metrics, destroys plaintext
-    versions, and exposes a special use case method for examining decrypts of
-    these files within the context management.
-    """
-
     def __init__(self, file_name, bury=True):
         self.bury = bury
         self.file_name = file_name
@@ -115,7 +103,7 @@ class EMPIFileCrypt:
         try:
             self.s3_resource.Object(self.s3_bucket, file_name).load()
             exists = True
-        except:  # ToDo: identify s3 error-scape
+        except:  # ToDo: implement s3 error-scape
             exists = False
 
         return exists
@@ -159,11 +147,6 @@ class EMPIFileCrypt:
 
 
 def empi_file_bury(file_name, read_out=False):
-    """
-    :param file_name: the plaintext EMPI metric file to be handled
-    :param read_out: set to True to get out with a file decrypt for analytics
-    :return response: the file_name of an encrypted file or the contents of a decrypted metric
-    """
     with EMPIFileCrypt(file_name) as empi_file:
         response = empi_file.encrypted_file_name  # type: ignore
         if read_out:
@@ -173,10 +156,6 @@ def empi_file_bury(file_name, read_out=False):
 
 
 def empi_file_disinter(encrypted_file_name):
-    """
-    :param encrypted_file_name: the encrypted EMPI metric file to be handled
-    :return decrypt: the contents of a decrypted metric
-    """
     file_name = encrypted_file_name.replace('_enc_', '')
     decrypt = None
     if file_name != encrypted_file_name:
